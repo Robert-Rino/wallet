5d27fed096894da4a56547edd2f0fbc00b8da2c2
robertrino <a901002666@gmail.com>
2022-04-10 01:31:45 +0800

    Remove create app

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgres/db"
+# TODO: Find better way.
+SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL') or 'postgresql://user:password@postgres/db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from app.exceptions import Unauthorized
-from fastapi import Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from typing import List, Optional
-# from .routes import users
-from . import routes, create_app
+from . import routes, models, database
 
-app = create_app()
+app = FastAPI()
+
 app.include_router(routes.router)
 app.include_router(routes.users.router)
 
+@app.on_event('startup')
+async def create_db_tables():
+    models.Base.metadata.create_all(bind=database.engine)
 
 @app.exception_handler(Unauthorized)
 async def unauthorized(request: Request, exc: Unauthorized):
